971a2900a92605f64027abe16ef93ff8b580abbf
fanxl <flittlehappy@.163.com>
2016-12-08 09:41:24 +0800

    no message

---
+++ yyyy/yyyy/model.h
@@ -0,0 +1,13 @@
+//
+//  model.h
+//  yyyy
+//
+//  Created by FLE on 2016/12/8.
+//  Copyright © 2016年 LZzzzz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface model : NSObject
+
+@end
---
+++ yyyy/yyyy/model.m
@@ -0,0 +1,13 @@
+//
+//  model.m
+//  yyyy
+//
+//  Created by FLE on 2016/12/8.
+//  Copyright © 2016年 LZzzzz. All rights reserved.
+//
+
+#import "model.h"
+
+@implementation model
+
+@end
